4d7030dd3a97afb34d9f47f056892c251f317742
Xavier Mitault <xavier.mitault@epitech.eu>
2023-07-23 12:06:04 +0200

    Pretty entrypoint

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+NORMAL='\e[0m'
+BOLD='\e[1m'
+GREEN='\e[32m'
+RED='\e[31m'
+
 BASE_URL='https://raw.githubusercontent.com/Saverio976/ducky-the-duck/main/script/'
 SCRIPTS=(
     'b-alias.sh'
@@ -12,10 +17,22 @@ function run_script() {
     curl -sLf "${BASE_URL}${script}" | bash
 }
 
-SCRIPT=''
-while [ ! "$SCRIPT" = 'quit' ]; do
-    select SCRIPT in "${SCRIPTS[@]}"; do
-        break
-    done < /dev/tty
-    run_script "$SCRIPT"
-done
+function main() {
+    echo -e "${BOLD}Ducky the Duck${NORMAL}"
+    local SCRIPT=''
+    while [ ! "$SCRIPT" = 'quit' ]; do
+        echo -e "${BOLD}Select what to launch:${NORMAL}"
+        select SCRIPT in "${SCRIPTS[@]}"; do
+            break
+        done < /dev/tty
+        if run_script "$SCRIPT"; then
+            echo -e "${GREEN}Done${NORMAL}"
+        else
+            echo -e "${RED}Error${NORMAL}"
+        fi
+        echo
+        echo
+    done
+}
+
+main
